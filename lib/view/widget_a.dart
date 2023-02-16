352273613202992d7c3ce89b8d0d820f646b3efe
hfjxjjd123 <licbusiness21@gmail.com>
2023-02-16 23:40:12 +0900

    기본템플릿 설계

---
+++ lib/view/widget_a.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class WidgetA extends StatelessWidget {
+  const WidgetA({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
