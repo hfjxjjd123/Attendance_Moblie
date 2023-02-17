55aae1852284ff734f7395c1065e8b5b6c56d59b
hfjxjjd123 <licbusiness21@gmail.com>
2023-02-17 21:33:10 +0900

    color를 const에 추가

---
+++ lib/const/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color color1 = Color(0xFF202126);
+Color color2 = Color(0xFF045159);
+Color color3 = Color(0xFFa2f2b6);
+Color color4 = Color(0xFFb3f2c3);
+Color backgroundColor = Color(0xFFf2f2f2);
