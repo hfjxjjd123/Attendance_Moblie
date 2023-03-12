a5c5f4a41bb1a95acb5d878b16ebdbb02f8def6a
hfjxjjd123 <licbusiness21@gmail.com>
2023-03-12 15:44:54 +0900

    api

--- lib/api/event_api.dart
+++ lib/api/event_api.dart
@@ -1,66 +1,52 @@
 
 
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
+import 'package:gdsc_uos_atttendance/dto/user_group_dto.dart';
 
 import '../domain/event.dart';
 
-class EventAPI{
+class EventApi{
   final dio = Dio(BaseOptions(
-    baseUrl: "https://localhost:8080/event"
+    baseUrl: "https://localhost:8080/user-group"
   ));
 
   //그룹id로 이벤트조회
-  Future<Event?> getEventByGroup(int groupId) async {
+  Future<UserGroupDto> getInvolvingGroup(int userId) async {
 
-    Response response = await dio.get("/group");
+    Response response = await dio.get("/groups");
 
     if (response.statusCode == 200) {
-      final json = response.data as Map<String, dynamic>;
-      Event event = Event.fromJson(json);
+      final json = jsonDecode(response.data);
 
-      return event;
+      return UserGroupDto.fromJson(json);
     }
     else {
-      //error 화면을 띄우는 것도 방법
-      return null;
+      //TODO error 처리로직 필요
+      return UserGroupDto.fromJson(null);
     }
 
   }
 
   //user_id로 host 이벤트 조회
-  Future<List<Event?>> getEventsByHost(int userId) async {
+  Future<List<Event>> getRecentEvents(List<int> groupIds) async {
 
-    Response response = await dio.get("/host");
+      Response response = await dio.post(
+          "/events", data: groupIds
+      );
 
-    if (response.statusCode == 200) {
-      final jsonList = response.data as List<Map<String, dynamic>>;
-      List<Event> eventList = jsonList.map((json) => Event.fromJson(json)).toList();
+      if(response.statusCode == 200){
+        final data = response.data as List<dynamic>;
+        return List<Event>.from(data.map((json)=>Event.fromJson(json)));
 
-      return eventList;
-    }
-    else {
-      //error message
-      return [];
-    }
+      }else{
+        //TODO exception 처리
+        return [];
+      }
 
   }
   //user_id로 partis 이벤트 조회
-  Future<List<Event>> getEventsByPartis(int userId) async {
-
-    Response response = await dio.get("/partis");
-
-    if (response.statusCode == 200) {
-      final jsonList = response.data as List<Map<String, dynamic>>;
-      List<Event> eventList = jsonList.map((json) => Event.fromJson(json)).toList();
-
-      return eventList;
-    }
-    else {
-      //error message
-      return [];
-    }
-
-  }
 
 
 }
